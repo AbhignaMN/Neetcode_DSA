44d1c2efb26e8126e997244d00f5bbb08514039f
Abby <abhigna2324@gmail.com>
2026-07-28 16:16:13 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+
+        if not node:
+            return None
+
+        visited = {}
+
+        def dfs(node):
+
+            if node in visited:
+                return visited[node]
+
+            copy = Node(node.val)
+
+            visited[node] = copy
+
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+
+            return copy
+
+        return dfs(node)
